f83a76f0c46a5da49008f64598caefad9a0f37f6
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-08-25 11:30:53 -0400

    Moteur, robot et Main

---
+++ Laboratoire_1/main.py
@@ -0,0 +1,69 @@
+#Alexandre Carle et Louis-philippe Rousseau
+#25 août 2022
+
+import gpiozero
+import numpy as np
+import cv2
+from Laboratoire_1.robot import Robot
+
+
+# Constantes
+# Touches de clavier
+W = 'w'
+Q = 'q'
+E = 'e'
+A = 'a'
+S = 's'
+D = 'd'
+SPACE = 'space'
+PLUS = '+'
+MOINS = '-'
+X = 'x'
+
+if(__name__ == "main"):
+    
+    quitter = False
+    
+    #Objet Robot
+    robot = Robot()
+    
+    img = np.zeros((512,512,3),np.uint8)
+    cv2.imshow('Labo_1',img)
+    
+    while (quitter):
+        key = cv2.waitKey(100) # 100 milliseconds
+        
+        if key == ord(W): 
+            robot.Avancer()
+
+        elif key == ord(Q):
+            robot.Avancer('g')
+            
+        elif key == ord(E):
+            robot.Avancer('d')
+            
+        elif key == ord(A):
+            robot.Tourner_90('g')
+        
+        elif key == ord(S):
+            robot.Reculer()
+        
+        elif key == ord(D):
+            robot.Tourner_90('d')
+        
+        elif key == ord(SPACE):
+            robot.Arreter()
+            
+        elif key == ord(PLUS):
+            robot.Augmenter_Vitesse()
+            
+        elif key == ord(MOINS):
+            robot.Diminuer_Vitesse()
+            
+        elif key == ord(X):
+            quitter = True
+            robot.Arreter()
+    
+    print("Au revoir!")
+            
+
---
+++ Laboratoire_1/robot.py
@@ -0,0 +1,69 @@
+#Alexandre Carle et Louis-philippe Rousseau
+#25 août 2022
+
+from Laboratoire_1.moteurs import Moteurs
+
+class Robot:
+    def init(self):
+        self.moteurs = Moteurs();
+        
+        
+    def Avancer(self, dir):
+        self.Arreter()
+        self.moteurs.IN1.on()
+        self.moteurs.IN3.on()
+        
+        if(dir == None or dir == " "):
+            self.moteurs.ENA.value = 0.3
+            self.moteurs.ENB.value = 0.3
+        elif(dir == "g"):
+            self.moteurs.ENA.value = 0.3
+            self.moteurs.ENB.value = 0.4
+        elif(dir == "d"):
+            self.moteurs.ENA.value = 0.4
+            self.moteurs.ENB.value = 0.3
+        
+        
+    def Reculer(self):
+        self.Arreter()
+        self.moteurs.IN2.on()
+        self.moteurs.IN4.on()
+        self.moteurs.ENA.value = 0.3
+        self.moteurs.ENB.value = 0.3
+            
+    def Tourner_90(self, dir):
+        self.Arreter()
+            
+        if (dir == "g"):
+            self.moteurs.IN2.on()
+            self.moteurs.IN3.on()
+            self.moteurs.ENA.value = 0.37
+            self.moteurs.ENB.value = 0.37
+        elif (dir == "d"):
+            self.moteurs.IN1.on()
+            self.moteurs.IN4.on()
+            self.moteurs.ENA.value = 0.37
+            self.moteurs.ENB.value = 0.37
+
+    def Arreter(self):
+        self.moteurs.ENA.off()
+        self.moteurs.ENB.off()
+        self.moteurs.IN1.off()
+        self.moteurs.IN2.off()
+        self.moteurs.IN3.off()
+        self.moteurs.IN4.off()
+    
+    
+    #10% == 0.1
+    
+    def Augmenter_Vitesse(self):
+        self.moteurs.ENA.value += 0.1 
+        self.moteurs.ENB.value += 0.1
+        print(self.moteurs.ENB.value)
+        print(self.moteurs.ENA.value)
+        
+    def Diminuer_Vitesse(self):
+        self.moteurs.ENA.value -= 0.1
+        self.moteurs.ENB.value -= 0.1
+        print(self.moteurs.ENB.value)
+        print(self.moteurs.ENA.value)
