14a917d965bbe501a52ed35a79783b61918e3f3b
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-08-25 11:35:27 -0400

    Moteur, robot et Main

--- Laboratoire_1/main.py
+++ Laboratoire_1/main.py
@@ -20,50 +20,49 @@ PLUS = '+'
 MOINS = '-'
 X = 'x'
 
-if(__name__ == "main"):
     
-    quitter = False
-    
-    #Objet Robot
-    robot = Robot()
-    
-    img = np.zeros((512,512,3),np.uint8)
-    cv2.imshow('Labo_1',img)
-    
-    while (quitter):
-        key = cv2.waitKey(100) # 100 milliseconds
-        print("Allo")
-        if key == ord(W): 
-            robot.Avancer()
+quitter = False
 
-        elif key == ord(Q):
-            robot.Avancer('g')
-            
-        elif key == ord(E):
-            robot.Avancer('d')
-            
-        elif key == ord(A):
-            robot.Tourner_90('g')
+#Objet Robot
+robot = Robot()
+
+img = np.zeros((512,512,3),np.uint8)
+cv2.imshow('Labo_1',img)
+
+while (quitter):
+    key = cv2.waitKey(100) # 100 milliseconds
+    print("Allo")
+    if key == ord(W): 
+        robot.Avancer()
+
+    elif key == ord(Q):
+        robot.Avancer('g')
         
-        elif key == ord(S):
-            robot.Reculer()
+    elif key == ord(E):
+        robot.Avancer('d')
         
-        elif key == ord(D):
-            robot.Tourner_90('d')
-        
-        elif key == ord(SPACE):
-            robot.Arreter()
-            
-        elif key == ord(PLUS):
-            robot.Augmenter_Vitesse()
-            
-        elif key == ord(MOINS):
-            robot.Diminuer_Vitesse()
-            
-        elif key == ord(X):
-            quitter = True
-            robot.Arreter()
+    elif key == ord(A):
+        robot.Tourner_90('g')
     
-    print("Au revoir!")
-            
+    elif key == ord(S):
+        robot.Reculer()
+    
+    elif key == ord(D):
+        robot.Tourner_90('d')
+    
+    elif key == ord(SPACE):
+        robot.Arreter()
+        
+    elif key == ord(PLUS):
+        robot.Augmenter_Vitesse()
+        
+    elif key == ord(MOINS):
+        robot.Diminuer_Vitesse()
+        
+    elif key == ord(X):
+        quitter = True
+        robot.Arreter()
+
+print("Au revoir!")
+        
 
