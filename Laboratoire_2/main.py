06e02bd18a74574124961fce214243b05464f8ba
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-09-05 22:30:23 -0400

    Changements

--- Laboratoire_2/main.py
+++ Laboratoire_2/main.py
@@ -21,7 +21,21 @@ sonars = Sonar(SGT, SDT, SGE, SDE, arreter)
 del_jaune = Dell(PORT_DEL_JAUNE, sonars, 'g', arreter)
 del_verte = Dell(PORT_DEL_VERTE, sonars, 'd', arreter)
 
+org = (0,0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 1
+font_color = (255, 255, 255)
+line_type = 2
+
 img = np.zeros((512,512,3),np.uint8)
 cv2.imshow('Labo 1',img)
+
+cv2.putText(img, 
+            "Sonar ", 
+            org, 
+            font, 
+            font_scale, 
+            font_color, 
+            line_type)
 #sonars.Demarrer()
 #sonars.Arreter()
