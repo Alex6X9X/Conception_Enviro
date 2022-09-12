7eba97d1a8cdba27f810824abd54f711e9cfb703
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-09-12 12:55:37 -0400

    Changements

--- Laboratoire_2/sonar.py
+++ Laboratoire_2/sonar.py
@@ -68,15 +68,16 @@ class Sonar:
         
         self.temps_inactif = self.echo_gauche.inactive_time
         
-        distance = (time.perf_counter() - self.compteur_distanceg - (self.temps_inactif + self.temps_actif)) * VITESSE_SON /2
-
+        distance = (time.perf_counter() - self.compteur_distanceg) * VITESSE_SON / 2
+        print(distance)
         self.distance_courante_gauche = self.calculer_moyenne_mobile(distance , self.tableau_distanceg)
         
     def sonar_deactiver_d(self):
 
         self.temps_inactif = self.echo_droite.inactive_time
         
-        distance = (time.perf_counter() - self.compteur_distanced - (self.temps_inactif + self.temps_actif)) * VITESSE_SON / 2
+        distance = (time.perf_counter() - self.compteur_distanced) * VITESSE_SON / 2
+        print(distance)
 
         self.distance_courante_droite = self.calculer_moyenne_mobile(distance , self.tableau_distanced) 
     
