c15d85a86874b78ac707ee848e5b5f99decc8b7e
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-09-15 11:09:49 -0400

    LP

--- Laboratoire_3/Odomètre.py
+++ Laboratoire_3/Odomètre.py
@@ -1,26 +1,41 @@
 import threading
 from time import sleep
 import gpiozero
-from symbol import pass_stmt
-DISTANCE_PAR_TRANSITION=22
+
+DISTANCE_PAR_TRANSITION=0.55
 class Odomètre:
     def __init__(self , port_out_gauche, port_out_droite):
         self.encodeur_gauche = gpiozero.DigitalInputDevice(port_out_gauche)
         self.encodeur_droite = gpiozero.DigitalInputDevice(port_out_droite)
+        self.stop = threading.Event()
+        self.nombre_transition_gauche =0
+        self.nombre_transition_droite =0
         pass
-    def avancer_distance(self,distance):
+    def avancer_distance(self,distance_voulue):
         ##installer callbacks avant de faire avancer
-
+        self.encodeur_gauche.when_activated = self.when_activated_gauche
+        self.encodeur_gauche.when_deactivated = self.when_deactivated_gauche
+        self.encodeur_droite.when_activated = self.when_activated_droite
+        self.encodeur_droite.when_deactivated = self.when_deactivated_droite
+        if(self.calculer_distance() == distance_voulue):
+            self.stop.set()
         pass
     def when_activated_gauche(self):
-        pass
+        self.nombre_transition_gauche += 1
     def when_deactivated_gauche(self):
-        pass
+        self.nombre_transition_gauche += 1
     def when_activated_droite(self):
-        pass
+        self.nombre_transition_droite += 1
     def when_deactivated_droite(self):
-        pass
+        self.nombre_transition_droite += 1
     def attendre(self):
-        pass
+        self.stop.wait()
+        self.encodeur_gauche.when_activated = None
+        self.encodeur_gauche.when_deactivated = None
+        self.encodeur_droite.when_activated = None
+        self.encodeur_droite.when_deactivated = None
+    def calculer_distance(self):
+        distance = ((self.nombre_transition_gauche + self.nombre_transition_droite) /2) * DISTANCE_PAR_TRANSITION
+        return distance
 
         
---
+++ Laboratoire_3/main.py
@@ -0,0 +1,34 @@
+#Alexandre Carle et Louis-philippe Rousseau
+#29 août 2022
+#Dernier changement le 12 septembre 2022
+
+from time import sleep
+from robot import Robot
+from Odomètre import Odomètre
+
+
+import cv2
+
+PORT_DEL_JAUNE = 10
+PORT_DEL_VERTE = 9
+SGT = 8   #Sonar Gauche Trigger...
+SDT = 21  
+SGE = 25  #Sonar Gauche Echo...
+SDE = 20
+
+arreter = False
+
+robot = Robot()
+odomètre = Odomètre(2,2)
+
+
+
+print("Appuyer sur la touche 'x' pour quitter le programme...")
+
+#Démarrage des threads
+
+
+while (not arreter):
+    key = cv2.waitKey(100)
+          
+   
