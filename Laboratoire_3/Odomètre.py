d7a052227df594ead8e0a433bc5a356f64a64454
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-09-15 10:23:01 -0400

    LP

---
+++ Laboratoire_3/Odomètre.py
@@ -0,0 +1,19 @@
+import threading
+from time import sleep
+import gpiozero
+from symbol import pass_stmt
+DISTANCE_PAR_TRANSITION=22
+class Odomètre:
+    def __init__(self , port_out_gauche, port_out_droite):
+        pass
+    def avancer_distance(distance):
+        ##installer callbacks avant de faire avancer
+        pass
+    def when_activated():
+        pass
+    def when_deactivated():
+        pass
+    def attendre():
+        pass
+
+        
