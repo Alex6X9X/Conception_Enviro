a32324cf8fde9f26a00e2b62cffd281269ed94e7
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-09-15 10:31:11 -0400

    LP

--- Laboratoire_3/Odomètre.py
+++ Laboratoire_3/Odomètre.py
@@ -5,15 +5,22 @@ from symbol import pass_stmt
 DISTANCE_PAR_TRANSITION=22
 class Odomètre:
     def __init__(self , port_out_gauche, port_out_droite):
+        self.encodeur_gauche = gpiozero.DigitalInputDevice(port_out_gauche)
+        self.encodeur_droite = gpiozero.DigitalInputDevice(port_out_droite)
         pass
-    def avancer_distance(distance):
+    def avancer_distance(self,distance):
         ##installer callbacks avant de faire avancer
+
         pass
-    def when_activated():
+    def when_activated_gauche(self):
         pass
-    def when_deactivated():
+    def when_deactivated_gauche(self):
         pass
-    def attendre():
+    def when_activated_droite(self):
+        pass
+    def when_deactivated_droite(self):
+        pass
+    def attendre(self):
         pass
 
         
