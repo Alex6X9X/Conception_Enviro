39a2ca89bf42b4b4de2cb0eb97dd24665352f5d7
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-09-19 12:56:48 -0400

    Changements Moteurs

--- Laboratoire_3/moteurs.py
+++ Laboratoire_3/moteurs.py
@@ -4,7 +4,10 @@
 
 import gpiozero
 
+TAUX_VITESSE = 0.1
+
 class Moteurs:
+    
     def __init__(self):
         self.IN1 = gpiozero.DigitalOutputDevice(6)
         self.IN2 = gpiozero.DigitalOutputDevice(5)  # moteur G
@@ -13,6 +16,73 @@ class Moteurs:
         self.IN3 = gpiozero.DigitalOutputDevice(15)
         self.IN4 = gpiozero.DigitalOutputDevice(14) # moteur D
         self.ENB = gpiozero.PWMOutputDevice(18)
+        
+    def avancer(self, dir = None):
+        self.arreter()
+        self.IN1.on()
+        self.IN3.on()
+    
+        if(dir == None):
+            self.ENA.value = 1.0
+            self.ENB.value = 1.0
+        elif(dir == "g"):
+            self.avancer_gauche()
+        elif(dir == "d"):
+            self.avancer_droite()
+    
+    def avancer_gauche(self):
+        self.IN2.on()
+        self.IN3.on()
+        self.ENA.value = 0.6
+        self.ENB.value = 0.8
+    
+    def avancer_droite(self):
+        self.IN1.on()
+        self.IN4.on()
+        self.ENA.value = 0.8
+        self.ENB.value = 0.6
+        
+    def reculer(self):
+        self.arreter()
+        self.IN2.on()
+        self.IN4.on()
+        self.ENA.value = 1.0
+        self.ENB.value = 1.0
+            
+    def tourner_90(self, dir):
+        self.arreter()
+            
+        if (dir == "g"):
+            self.IN2.on()
+            self.IN3.on()
+            self.ENA.value = 0.5
+            self.ENB.value = 0.5
+        elif (dir == "d"):
+            self.IN1.on()
+            self.IN4.on()
+            self.ENA.value = 0.5
+            self.ENB.value = 0.5
+
+    def arreter(self):
+        self.ENA.off()
+        self.ENB.off()
+        self.IN1.off()
+        self.IN2.off()
+        self.IN3.off()
+        self.IN4.off()
+    
+    
+    #10% == 0.1
+    
+    def augmenter_Vitesse(self):
+        if(self.ENA.value + TAUX_VITESSE < 1):
+            self.ENA.value = self.ENA.value + TAUX_VITESSE 
+            self.ENB.value = self.ENB.value + TAUX_VITESSE
+        
+    def diminuer_Vitesse(self):
+        if(self.moteurs.ENB.value - TAUX_VITESSE > 0):
+            self.ENA.value = self.ENA.value - TAUX_VITESSE
+            self.ENB.value = self.ENB.value - TAUX_VITESSE
         
 
             
--- Laboratoire_3/robot.py
+++ Laboratoire_3/robot.py
@@ -10,64 +10,21 @@ class Robot:
         self.moteurs = Moteurs()
         
         
-    def Avancer(self, dir = None):
-        self.Arreter()
-        self.moteurs.IN1.on()
-        self.moteurs.IN3.on()
-        
-        if(dir == None):
-            self.moteurs.ENA.value = 1.0
-            self.moteurs.ENB.value = 1.0
-        elif(dir == "g"):
-            self.moteurs.IN2.on()
-            self.moteurs.IN3.on()
-            self.moteurs.ENA.value = 0.6
-            self.moteurs.ENB.value = 0.8
-        elif(dir == "d"):
-            self.moteurs.IN1.on()
-            self.moteurs.IN4.on()
-            self.moteurs.ENA.value = 0.8
-            self.moteurs.ENB.value = 0.6
+    def Avancer(self, dir):
+        self.moteurs.avancer(dir)
         
         
     def Reculer(self):
-        self.Arreter()
-        self.moteurs.IN2.on()
-        self.moteurs.IN4.on()
-        self.moteurs.ENA.value = 1.0
-        self.moteurs.ENB.value = 1.0
+        self.moteurs.reculer()
             
     def Tourner_90(self, dir):
-        self.Arreter()
-            
-        if (dir == "g"):
-            self.moteurs.IN2.on()
-            self.moteurs.IN3.on()
-            self.moteurs.ENA.value = 0.5
-            self.moteurs.ENB.value = 0.5
-        elif (dir == "d"):
-            self.moteurs.IN1.on()
-            self.moteurs.IN4.on()
-            self.moteurs.ENA.value = 0.5
-            self.moteurs.ENB.value = 0.5
+        self.moteurs.tourner_90(dir)
 
     def Arreter(self):
-        self.moteurs.ENA.off()
-        self.moteurs.ENB.off()
-        self.moteurs.IN1.off()
-        self.moteurs.IN2.off()
-        self.moteurs.IN3.off()
-        self.moteurs.IN4.off()
-    
-    
-    #10% == 0.1
+        self.moteurs.arreter()
     
     def Augmenter_Vitesse(self):
-        if(self.moteurs.ENA.value + 0.1 < 1):
-            self.moteurs.ENA.value = self.moteurs.ENA.value + 0.1 
-            self.moteurs.ENB.value = self.moteurs.ENB.value + 0.1
+        self.moteurs.augmenter_Vitesse()
         
     def Diminuer_Vitesse(self):
-        if(self.moteurs.ENB.value - 0.1 > 0):
-            self.moteurs.ENA.value = self.moteurs.ENA.value - 0.1
-            self.moteurs.ENB.value = self.moteurs.ENB.value - 0.1
+        self.moteurs.diminuer_Vitesse()
