3fa97288b6ffcacf7ded9915b0c9a82e323a7dac
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-09-19 13:20:13 -0400

    Changements Moteurs

--- Laboratoire_3/moteurs.py
+++ Laboratoire_3/moteurs.py
@@ -23,8 +23,8 @@ class Moteurs:
         self.IN3.on()
     
         if(dir == None):
-            self.ENA.value = 0.3
-            self.ENB.value = 0.3
+            self.ENA.value = 0.7
+            self.ENB.value = 0.7
         elif(dir == "g"):
             self.avancer_gauche()
         elif(dir == "d"):
