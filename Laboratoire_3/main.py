6c46bd8a3cee0efa1f409a95bfa09686c5842915
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-09-21 17:08:42 -0400

    Elimination de déchets

--- Laboratoire_3/main.py
+++ Laboratoire_3/main.py
@@ -6,18 +6,10 @@ from time import sleep
 from robot import Robot
 from Odomètre import Odomètre
 
-PORT_DEL_JAUNE = 10
-PORT_DEL_VERTE = 9
-SGT = 8   #Sonar Gauche Trigger...
-SDT = 21  
-SGE = 25  #Sonar Gauche Echo...
-SDE = 20
-
 arreter = False
 
 robot = Robot()
 odomètre = Odomètre(27,22)
-
 
 
 while (not arreter):
