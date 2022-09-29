e88d14a7cf7bf312fa4d71ccadcaca64287d1d3e
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-09-29 10:47:53 -0400

    LP

--- Laboratoire_4/Camera.py
+++ Laboratoire_4/Camera.py
@@ -10,6 +10,7 @@ SAT_MAX =202
 VAL_MIN = 64
 VAL_MAX = 255
 DELTA = 3
+EPAISSEUR = 2
 
 class Camera:
     
@@ -21,6 +22,7 @@ class Camera:
         self.frame_hsv = None
         self.vcap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
         self.vcap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+        self.contours = None
         
     def _read_(self):
         self.ok , self.image = self.vcap.read()
@@ -28,8 +30,35 @@ class Camera:
         teinte_min = np.array([TEINTE - DELTA, SAT_MIN, VAL_MIN])
         teinte_max = np.array([TEINTE + DELTA, SAT_MAX, VAL_MAX])
         self.image = cv2.inRange(self.image, teinte_min, teinte_max)
-
+        self._contour_()
         return self.image
+    def _contour_(self):
+        self.contours, _ = cv2.findContours(self.image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        plus_grand_aire = 0
+        x2 , y2 , l2 , h2 = None
+        for c in self.contours:
+            x, y, l, h = cv2.boundingRect(c)
+            air_rect = l * h
+            if(air_rect > plus_grand_aire):
+                plus_grand_aire = air_rect
+                x2 = x
+                y2 = y 
+                l2 = l
+                h2 = h  
+
+        self._draw_rectangle(x2,y2,l2,h2)   
+            
+        self._draw_contour_()
+        
+
+    def _draw_contour_(self ):
+        cv2.drawContours(self.image, self.contours, -1, (0,0,255), EPAISSEUR)
+    def _draw_rectangle(self,x,y,l,h):
+        cv2.rectangle(self.image, (x,y), (x+l,y+h), (255, 165, 0), EPAISSEUR) 
+
+
+
+
     
     
 
