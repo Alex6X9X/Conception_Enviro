c093cae0f5d72a2064699feb94ad3f9a5dfec5bc
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-10-05 13:22:39 -0400

    LP

--- Laboratoire_4/main.py
+++ Laboratoire_4/main.py
@@ -1,14 +1,15 @@
 import cv2
 import numpy as np
 from Camera import Camera
-
+from robot import Robot
 
 vcap = Camera()
+robot = Robot(vcap)
 
 while True:    
     vcap._read_()
     cv2.imshow("Image disc", vcap.image)
-
+    robot.DeterminerMouvement()
     choix = cv2.waitKey(125)
     if  choix == ord('q'):
         break    
