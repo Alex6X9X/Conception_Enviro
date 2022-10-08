90992473a97cd31732bf1dd993d429d253221782
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-10-08 11:34:10 -0400

    AC

--- Laboratoire_4/Camera.py
+++ Laboratoire_4/Camera.py
@@ -6,7 +6,7 @@ WIDTH = 320
 HEIGHT = 240
 PORT = 0
 TEINTE = 0
-SAT_MIN =120
+SAT_MIN = 120
 SAT_MAX = 213
 VAL_MIN = 107
 VAL_MAX = 255
@@ -29,13 +29,11 @@ class Camera:
         if not self.vcap.isOpened():
             print("Erreur lors de l'ouverture de la caméra")
             exit()
-        self.frame_hsv = None
         self.vcap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
         self.vcap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
         self.contours = None
         self.aire_balle = 0
         self.x_balle = None
-        #self.y_balle = None
         
     def _read_(self):
         self.ok , self.image = self.vcap.read()
@@ -68,8 +66,7 @@ class Camera:
         self.contours, _ = cv2.findContours(self.image, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
         plus_grand_aire = 0
         coordoné = []
-        #if(len(self.contours) == 0):
-            #self.aire_balle = 0
+
         for c in self.contours:
             x, y, l, h = cv2.boundingRect(c)
             air_rect = l * h
