fc18660f7b5c89fa7330a3c79a36c6c587820a83
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-10-13 15:06:16 -0400

    AC

--- Laboratoire_4/main.py
+++ Laboratoire_4/main.py
@@ -3,17 +3,21 @@ import cv2
 import numpy as np
 from Camera import Camera
 from robot import Robot
+from Console import Console
 
 vcap = Camera()
 robot = Robot(vcap)
+console = Console()
 
 arreter = False
 
 while not arreter:    
     vcap._read_()
+    console.afficher_image("Labo 4", np.zeros((175,175,3),np.uint8))
     robot.DeterminerMouvement()
     choix = cv2.waitKey(125)
     if  choix == ord('q'):
         arreter = True   
 
 vcap._release_()
+console.detruire_fenetres()
