c52648d8b8b42266cf19f86e918ba2a5a4e66b86
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-10-17 14:55:18 -0400

    LP

--- Laboratoire_5/Camera.py
+++ Laboratoire_5/Camera.py
@@ -115,7 +115,7 @@ class Camera:
         cv2.imwrite("image_modele.bmp", modele)
     
     def _trouver_image_modele_(self):
-        modele_minimise = cv2.imread("image_modele_version2" , 0)
+        modele_minimise = cv2.imread("image_modele_version2.bmp" , 0)
         mask = cv2.imread("background.png")
         w, h = modele_minimise.shape[::-1]
         res = cv2.matchTemplate(self.image, modele_minimise, cv2.TM_CCOEFF_NORMED, None, mask)
