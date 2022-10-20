84924717dc66516fa99461c52faae11b5d13b96f
unknown <a.carle2019@gmail.com>
2022-10-20 11:45:10 -0400

    AC

--- Laboratoire_5/main.py
+++ Laboratoire_5/main.py
@@ -4,12 +4,17 @@ from Camera import Camera
 
 
 vcap = Camera()
-
+arreter = False
+    
 frame = []
 #vcap._creation_modele_()
+
 vcap._trouver_image_modele_(frame)
 
-
+while not arreter:
+    choix = cv2.waitKey(100)
+    if  choix == ord('q'):
+        arreter = True  
 
 vcap.vcap.release()
 cv2.destroyAllWindows()
