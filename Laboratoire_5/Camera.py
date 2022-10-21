d9387f0996ed3f8b4e4150c4c9279d76a63bc241
unknown <a.carle2019@gmail.com>
2022-10-20 21:04:55 -0400

    Optimisation AC

--- Laboratoire_5/Camera.py
+++ Laboratoire_5/Camera.py
@@ -117,7 +117,7 @@ class Camera:
         modele_minimise = cv2.imread("image_modele_version2.bmp" , 0)
         mask = cv2.imread("background.png" , 0)
         
-        if(self.frame_roi == None):
+        if(any(self.frame_roi)):
             self.image =cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
             res = cv2.matchTemplate(self.image, modele_minimise, cv2.TM_CCOEFF_NORMED)
             self.min_val, self.max_val, self.min_loc, self.max_loc = cv2.minMaxLoc(res)
