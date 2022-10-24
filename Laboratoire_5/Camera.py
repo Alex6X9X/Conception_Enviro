2ba8b14da73b76fb8a30e8d4054d33294abffd5c
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-10-24 14:15:29 -0400

    LP

--- Laboratoire_5/Camera.py
+++ Laboratoire_5/Camera.py
@@ -96,8 +96,9 @@ class Camera:
         self.ymax = self.h + DELTA_ROI * 2
         self.xmax = self.l + DELTA_ROI * 2
         image_frame = self.image[self.ymin:self.ymax, self.xmin:self.xmax]
-        print(image_frame)
+        
         if(image_frame !=[]):
+            print(image_frame)
             self.frame_roi = cv2.cvtColor(image_frame, cv2.COLOR_BGR2GRAY)
 
         
