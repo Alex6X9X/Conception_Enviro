341750b4eb2ca8d30659229f6bb7eee71f662699
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-10-31 14:03:23 -0400

    AC

--- Laboratoire_6/Navigation.py
+++ Laboratoire_6/Navigation.py
@@ -46,8 +46,6 @@ class Navigation :
                 ##À l’arrêt: le fil calcule les biais de gx et de ay en utilisant une moyenne fenêtrée. 
                 self._biais_gx = calculer_moyenne_mobile(self.gx , self._tab_biais_gx)
                 self._biais_ay = calculer_moyenne_mobile(self.ay , self._tab_biais_ay)
-                print(self._biais_ay)
-                print(self._biais_gx)
                 print("immobile")
                 
             elif(self.état ==  State.Rotation):
@@ -57,7 +55,6 @@ class Navigation :
                 self.gx_precedent = self.gx
                     
                 print(self.angleX)
-                print(self.gx)
                 
             elif(self.état ==  State.Translation):
                 ##En translation: le fil calcule la nouvelle position en y du robot en tenant compte du temps écoulé entre deux mesures et le biais calculé pour ay. 
