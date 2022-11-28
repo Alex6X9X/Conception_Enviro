c24e8e6ff108eee06eeed5da4e7fd7210d8f7bc8
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-11-28 14:29:40 -0500

    L

--- PFI/main.py
+++ PFI/main.py
@@ -10,41 +10,54 @@ from Lidar import Lidar
 en_marche = True
 
 imu = ICM20948()
-
+current_angle = 0
 navigation = Navigation(imu, en_marche)
 radioNavigation = RadioNavigation(en_marche)
 radioNavigation.demarrerCommunication()
 radioNavigation.thread_get_position.start()
 robot = Robot(navigation , radioNavigation, en_marche)
 robot.initialiserPosition()
+current_angle = navigation.angleX
 #lidar = Lidar(en_marche)
 
 #robot.Tourner(0)
 #if(navigation.angleX < -90):
 #    robot.Freiner()
     
-tabPosition = [0, 7.80, 1.60, 6]
+tabPosition = [0,7.80, 1.60, 6]
 tabAxes = ['Y' , 'X' , 'Y' , 'X']
 index = 0 
 while en_marche:
-    sleep(0.1)
-    print("x")
-    print(str(radioNavigation.x))
-    print("y")
-    print(str(radioNavigation.y))
-    print("---")
+    
+    ##sleep(0.1)
+    ##print("x")
+    ##print(str(radioNavigation.x))
+    ##print("y")
+    ##print(str(radioNavigation.y))
+    ##print("---")
+
+    
+    robot.Tourner(0)
+    print(abs(navigation.angleX - current_angle))
+    if(abs(navigation.angleX - current_angle) >= 90):
+        robot.Freiner()
+        current_angle = navigation.angleX
+
+    
+
     #lidar.ScanLidar()
     #lidar.GetDistance(0)
     ##robot.Avancer()
 
 
-    ##if(not robot.has_started):
-      ##  robot.CalculerDistance(tabPosition[index] , tabAxes[index])
-      ##  robot.Start_Thread_Avancer()
-      ##  index += 1
-      ##  robot.has_started = True
+  ##  if(not robot.has_started):
+       ## robot.CalculerDistance(tabPosition[index] , tabAxes[index])
+       ## robot.Start_Thread_Avancer()
+       ## index += 1
+        ##robot.has_started = True
     ##if(robot.arriver_position):
       ##  robot.Stop_Thread_Avancer()
+        
     ##ici ajouter vérification du Lidar pour vérifier les obstacles et arrêter le robot si c'est le cas
     ##if(robot.obstacle_in_the_way):
       ##   robot.PauseObstacle()
