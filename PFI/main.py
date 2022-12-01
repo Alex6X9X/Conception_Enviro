522e534d0082a7ed35fca8ed09db9e0a43071d9d
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-01 11:22:32 -0500

    AC

--- PFI/main.py
+++ PFI/main.py
@@ -33,7 +33,7 @@ sleep(TEMPS_CALIBRATION)
 #    robot.Freiner()
     
 tabPosition = [0,7.80, 1.60, 6]
-tabPositionTest = [6,7.80, 1.60, 6]
+tabPositionTest = [5,7.80, 1.60, 6]
 tabAxes = [Axe.Y , Axe.X , Axe.Y , Axe.X]
 
 index = 0 
