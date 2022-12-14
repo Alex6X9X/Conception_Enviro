88d18c8b3042dc3141faf5b166a1759d50161cb4
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-14 15:48:40 -0500

    L

--- PFI/main.py
+++ PFI/main.py
@@ -26,7 +26,7 @@ beggining_of_circuit = True
 
 ##tabPosition = [(6 , -0.34), (7.94 , 0.27) , (8.15 , 2.63) , (6.2 , 2.78)]
 tabPosition = [(12.20 , 5.80) , (12.57 , 7.04)]
-has_started = True
+has_started = False
 angle = 0
 index = 0 
 robot.initialiserPosition()
