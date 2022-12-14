4f4387ff996de9fc79a430de8d5f50a5f31b0a53
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-14 13:41:19 -0500

    L

--- PFI/RadioNavigation.py
+++ PFI/RadioNavigation.py
@@ -14,8 +14,6 @@ class RadioNavigation:
         self.ser.open()
         print("RadioNavigation")
         self.ser.write(b'\r\r') # séquence d’octets
-        time.sleep(1)
-        self.ser.write(b'lep\r')
         self.en_marche = en_marche
         self.data = None
         self.x = 0
@@ -26,7 +24,7 @@ class RadioNavigation:
     def getPosition(self):
         while(self.en_marche):
             time.sleep(0.1)
-
+            self.ser.write(b'lep\r')
             self.data = str(self.ser.readline())
             print(self.data)
             arrayString = self.data.split(',') 
