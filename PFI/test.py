d57f4be0d0e409889e061d4770c2c9645120a85e
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-14 13:43:49 -0500

    L

--- PFI/test.py
+++ PFI/test.py
@@ -10,25 +10,30 @@ from Lidar import Lidar
 from State import State
 from Direction import Direction
 
-TEMPS_CALIBRATION = 5
+
 en_marche = True
 
-
-
-
+imu = ICM20948()
+angle = 0
+navigation = Navigation(imu, en_marche)
 radioNavigation = RadioNavigation(en_marche)
+lidar = Lidar(en_marche)
+lidar.thread_scan_lidar.start()
+radioNavigation.thread_get_position.start()
+robot = Robot(navigation , radioNavigation, lidar, en_marche)
+
 
 
 beggining_of_circuit = True
 
-sleep(TEMPS_CALIBRATION)
+sleep(5)
 
 ##tabPosition = [(6 , -0.34), (7.94 , 0.27) , (8.15 , 2.63) , (6.2 , 2.78)]
 tabPosition = [(12.76 , 5.73)]
 has_started = False
 
 sleep(10)
-
+robot.Avancer()
         
 radioNavigation.thread_get_position.join()
 radioNavigation.fermerConnection()
