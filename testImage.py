80c3cf50861ddb884b3c67b982a7e1a35a739f55
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-14 22:26:15 -0500

    LP

---
+++ testImage.py
@@ -0,0 +1,21 @@
+#Auteurs: Alexandre Carle et Louis-philippe Rousseau
+#Dernier changement 13 octobre 2022
+
+import cv2
+import numpy as np
+
+WIDTH = 320
+HEIGHT = 240
+PORT = 0
+TEINTE = 0
+SAT_MIN = 120
+SAT_MAX = 213
+VAL_MIN = 107
+VAL_MAX = 255
+DELTA = 10
+EPAISSEUR = 2
+image = []
+cv2.imshow("Image", image)
+cv2.waitKey(0) 
+
+
