1228591682e20fcfd87df92161ae02d076d37b73
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-15 12:56:43 -0500

    LP

--- PFI/test.py
+++ PFI/test.py
@@ -46,8 +46,8 @@ while(en_marche):
             has_started_turning = True
         
         ##verif angle 90
-        print(navigation.angleX)
-        print(robot.angle)
+        print("nav" + navigation.angleX)
+        print("robot" + robot.angle)
         if(navigation.angleX >= robot.angle + 90):
             robot.Freiner()
 
