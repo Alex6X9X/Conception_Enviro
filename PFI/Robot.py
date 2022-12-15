1600178d868b8de8e7e824ab01616aec15336d6e
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-15 13:20:19 -0500

    AC

--- PFI/Robot.py
+++ PFI/Robot.py
@@ -40,6 +40,7 @@ class Robot :
         self.y = self.radioNavigation.y
                 
     def Start_Thread_Avancer(self, prochainX, prochainY, angle_depart):
+        self.avance = True
         self.thread_avancer = threading.Thread(target = self.AvancerToPosition , args=(prochainX, prochainY, angle_depart))
         self.thread_Calculer_Distance_Parcourue = threading.Thread(target = self.CalculerDistanceParcourue , args=(prochainX, prochainY))     
         self.thread_avancer.start()
