214f06591668ca3ba079e6e6beb0fdeacc1c2b9c
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-15 12:07:09 -0500

    AC

--- PFI/test.py
+++ PFI/test.py
@@ -25,7 +25,7 @@ robot = Robot(navigation , radioNavigation, lidar, en_marche)
 beggining_of_circuit = True
 
 ##tabPosition = [(6 , -0.34), (7.94 , 0.27) , (8.15 , 2.63) , (6.2 , 2.78)]
-tabPosition = [(12.50 , 5.80) , (12.57 , 7.04)]
+tabPosition = [(12.20 , 5.80) , (12.57 , 7.04)]
 has_started = False
 sleep(7)
 robot.initialiserPosition()
