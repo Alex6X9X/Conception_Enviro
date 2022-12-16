5a642e189d16ac4157d2ed0e7c64395f9226880a
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-16 15:06:27 -0500

    LP

--- PFI/moteurs.py
+++ PFI/moteurs.py
@@ -28,8 +28,8 @@ class Moteurs:
         self.IN1.on()
         self.IN3.on()
     
-        self.ENA.value = 0.23
-        self.ENB.value = 0.23
+        self.ENA.value = 0.21
+        self.ENB.value = 0.21
     #En theorie Reculer
     def reculer(self):
         self.arreter()
