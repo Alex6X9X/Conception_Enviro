0b9e350c6ae389378d66838657a17ee6c1b78ba3
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-16 14:19:14 -0500

    AC

--- PFI/Lidar.py
+++ PFI/Lidar.py
@@ -21,7 +21,7 @@ class Lidar:
             self.gen = self.Obj.StartScanning()
             while (self.en_marche):         
                 self.data = next(self.gen) # Dictionnaire: data[0:359]   
-                print(self.data)            
+                print(self.data[150])            
                 time.sleep(0.5)
 
             self.Obj.StopScanning()
