424a5e52aba4090cbd48f48ebf02b6cdafa956c7
Louis-Philippe Rousseau <louis-philippe_rousseau@msn.com>
2022-12-16 14:28:36 -0500

    LP

--- PFI/Robot.py
+++ PFI/Robot.py
@@ -116,7 +116,7 @@ class Robot :
     def VerifierDistanceLidar(self):
         distance = self.lidar.GetDistance()
         print("distance" , distance)
-        if(distance != None):
+        if(distance != None or distance != []):
             if(isinstance(distance , int)):
                  return distance <= 1000
             else:
