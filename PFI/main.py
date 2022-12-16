2659ef875638b4abf4f07b6e9f24043ca16a0192
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-16 14:58:27 -0500

    AC

--- PFI/main.py
+++ PFI/main.py
@@ -18,7 +18,7 @@ angle = 0
 navigation = Navigation(imu, en_marche)
 radioNavigation = RadioNavigation(en_marche)
 lidar = Lidar(en_marche)
-lidar.thread_scan_lidar.start()
+#lidar.thread_scan_lidar.start()
 robot = Robot(navigation , radioNavigation, lidar, en_marche)
 
 beggining_of_circuit = True
