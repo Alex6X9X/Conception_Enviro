548a44954995e6e6088ec55ead9326ae50c1a335
Alexandre <98857863+Alex6X9X@users.noreply.github.com>
2022-12-19 13:24:42 -0500

    AC

--- PFI/Robot.py
+++ PFI/Robot.py
@@ -92,7 +92,7 @@ class Robot :
             sleep(0.01)
             obstacleInTheWay = self.VerifierDistanceLidar()
             print("obstacle" , obstacleInTheWay)
-            while(obstacleInTheWay):
+            while(self.VerifierDistanceLidar()):
                 self.Freiner()
                 self.IsStopped = True
                 
